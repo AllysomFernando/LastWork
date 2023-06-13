1fb09248e1e8c0f25e4e8776b9126f286d871f14
AllysomDev <afluis@fag.edu.br>
2023-06-13 18:23:04 -0300

    asaufdgfasdf

--- src/main/java/fag/lastWork/alimento/Alimento.java
+++ src/main/java/fag/lastWork/alimento/Alimento.java
@@ -2,7 +2,34 @@ package fag.lastWork.alimento;
 
 import fag.lastWork.produtoBase.ProdutoBase;
 
-public class Alimento extends ProdutoBase {
+public class Alimento {
     private String preco;
     private String nome;
+
+    private Integer quantidade;
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
 }
--- src/main/java/fag/lastWork/livro/Livro.java
+++ src/main/java/fag/lastWork/livro/Livro.java
@@ -2,11 +2,13 @@ package fag.lastWork.livro;
 
 import fag.lastWork.produtoBase.ProdutoBase;
 
+import java.util.List;
+
 public class Livro extends ProdutoBase {
     private String nome;
     private String autor;
-    private String preço;
-    private Enum genero; //criado um enum importar dps
-    private Enum editora; //criado um enum importar dps
+    private String preco;
+    private List genero; //criado um enum importar dps
+    private List editora; //criado um enum importar dps
     private int disponibilidade;
 }
