cb2dc6dd124c198a7dca216f78d2937582c55dac
AllysomDev <afluis@fag.edu.br>
2023-06-14 17:29:51 -0300

    mudei alguma coisa

--- src/main/java/fag/lastWork/alimento/Alimento.java
+++ src/main/java/fag/lastWork/alimento/Alimento.java
@@ -1,12 +1,48 @@
 package fag.lastWork.alimento;
 
-import fag.lastWork.produtoBase.ProdutoBase;
+import java.util.Scanner;
 
 public class Alimento {
+    Scanner scanner = new Scanner(System.in);
     private String preco;
     private String nome;
-
     private Integer quantidade;
+
+
+    public Alimento(String preco, String nome, Integer quantidade){
+        this.preco = preco;
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+    public void lerNome(){
+        System.out.println("Qual é o nome do seu alimento?");
+        String nome = scanner.nextLine();
+        setNome(nome);
+    }
+    public void lerPreco(){
+        System.out.println("Qual é o preço do seu alimento?");
+        String preco = scanner.nextLine();
+        setPreco(preco);
+    }
+
+    public void lerQuantidade(){
+        System.out.println("Qual é a quantidade do seu alimento?");
+        String preco = scanner.nextLine();
+        setPreco(preco);
+    }
+
+    public static void gerarAlimento(){
+        Alimento alimento = new Alimento(null,null,null);
+        alimento.lerPreco();
+        alimento.lerNome();
+        alimento.lerQuantidade();
+    }
+
+
+    public static void updateAlimento(){
+        System.out.println("Qual é o alimento que você quer deletar?");
+        gerarAlimento();
+    }
 
     public String getPreco() {
         return preco;
